c73238c73c19c42eb5d5a156b6a6dc5276770b3e
Bar <miliavsky@gmail.com>
2017-07-29 11:28:23 +0300

    feat(logo): add canvas logo

--- client/app/shell/shell.controller.js
+++ client/app/shell/shell.controller.js
@@ -4,11 +4,15 @@ const MODULE_NAME = 'ipoke.controllers';
 
 angular.module(MODULE_NAME)
     .controller('shell', ($scope, Auth) => {
+
+        createLogoInCanvas();
       $scope.login = () => {
         Auth.login({username: $scope.userName, password: $scope.password});
       };
 
       $scope.getUser = () => Auth.getCurrentUser();
+
+        createLogoInCanvas();
 
       $scope.isLoggedOn = () => {
         return Auth.getCurrentUser();
@@ -18,3 +22,13 @@ angular.module(MODULE_NAME)
         return Auth.getCurrentUser() && Auth.getCurrentUser().admin;
       };
     });
+
+function createLogoInCanvas() {
+    const canvas = document.getElementById('logoCanvas');
+    const ctx = canvas.getContext('2d');
+    ctx.font = '36px Roboto';
+    ctx.fillStyle = "#ffffff";
+    ctx.strokeStyle = "#ffffff";
+    ctx.fillText('IPoke', canvas.width / 2, 45);
+    // ctx.strokeText('IPoke', canvas.height / 2, 50);
+}
