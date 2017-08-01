12414bf95d05e9da35c4cf4e1b565d2213781741
Nadav Kaner <Nadavkaner1@gmail.com>
2017-08-02 00:38:40 +0300

    feat(socket-io): initialize socket io in server and client side

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import mongooseConfig from './config/mongoose';
 import http from 'http';
 import express from 'express';
+import io from 'socket.io';
 import expressConfig from './config/express';
 
 const mongoInitPromise = mongooseConfig(mongoose);
@@ -11,8 +12,13 @@ mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 const server = http.createServer(app);
+const socket = io.listen(server);
 
 expressConfig(app);
+
+socket.on('connect', () => {
+  console.log('a user connected');
+});
 
 const expressPromise = new Promise(resolve => {
   server.listen(process.env.PORT, () => {
